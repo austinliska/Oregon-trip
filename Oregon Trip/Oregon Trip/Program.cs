08b60af8087e211e1fd90fa71958cdb82663d486
Kevin Mahoney <kevcube@gmail.com>
2016-09-30 11:45:28 -0600

    rewrote program class..

--- Oregon Trip/Oregon Trip/Program.cs
+++ Oregon Trip/Oregon Trip/Program.cs
@@ -10,6 +10,43 @@ namespace Oregon_Trip
     {
         static void Main(string[] args)
         {
+            Event Mexican_Standoff = new Event();
+            Mexican_Standoff.set_shit("fd","fd",[0, 0, 0, 3, 0, 0],
         }
     }
+    class Event
+    {
+        string start_str; //started event, passed requirements
+        string missed_str; //insufficient stats
+        string fail_str; //failure by chance, not stats
+        string end_str; //success
+        int[] stat_req = new int[6]; /*
+                        Money 0
+                        Intelligence 1
+                        Charisma 2
+                        Strength 3
+                        Perception 4
+                        Luck 5
+                        [0,0,0,3,0,0] requires 3 strength, 0 all other stats
+                         */
+
+        int[] stat_change = new int[6];
+        bool has_choices;
+
+        public void set_shit(string start_str, string missed_str, string fail_str, string end_str, int[] stat_req, int[] stat_change, bool has_choices = false)
+        {
+            start_str = this.start_str;
+            missed_str = this.missed_str;
+            fail_str = this.fail_str;
+            end_str = this.end_str;
+            stat_req = this.stat_req;
+            stat_change = this.stat_change;
+            has_choices = this.has_choices;
+        }
+    }
+
+    
+
+
+
 }
